d3f460c3af52e8bf472c4c2ee3d740c4126451ff
Purnashree <97431127+PurnashreeTusharGawade@users.noreply.github.com>
2024-02-28 19:46:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var mainImg=document.getElementById("main-image");
+var smallImg=document.getElementsByClassName("small-img");
+smallImg[0].onclick = function(){
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    mainImg.src = smallImg[2].src;
+}
